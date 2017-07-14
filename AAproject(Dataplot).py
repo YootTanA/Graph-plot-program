4890bfb5621760f02ea5b31cf1ef9f51ec8ccad1
YootTanA <oat_boonpalit@hotmail.com>
2017-07-14 18:19:50 +0700

    Add files via upload

---
+++ AAproject(Dataplot).py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+
+def graph_plot(x,y):
+    plt.plot(x,y)
+    plt.show()
+def graph_name(name):
+    plt.title(name)
+def graph_label(xLa,yLa):
+    plt.xlabel(xLa)
+    plt.ylabel(yLa)
+    
+#เริ่มต้น โปรแกรม
+while(True):
+    n = int(input("press button 1 for plot graph \npress button 2 for end a program\n:"))
+    if(n==1):
+#ถามชื่อกราฟ
+      while(True):
+          yes_no_quest = input("Do you want to name your graph(yes/no)?\n:")
+          if(yes_no_quest=='Yes' or yes_no_quest=='yes'):
+              nameG = input('Input your graph\'s name\n:')
+              graph_name(nameG)
+              break
+          elif(yes_no_quest=='No' or yes_no_quest=='no'):
+              print("ok let plot a graph with out name \n")
+              break
+          else:
+              print("Wrong input!! Try again")
+#ถามชื่อ เส้น x และ เส้น y
+      while(True):      
+          yes_no_quest2= input("Do you want to name xlabels and ylabels(yes/no)?\n:")
+          if(yes_no_quest2 =='Yes' or yes_no_quest2=='yes'):
+              name_xla = input('Input your xlabel name\n:')
+              name_yla = input('Input your ylabel name\n:')
+              graph_label(name_xla,name_yla)
+              break
+          elif((yes_no_quest=='No' or yes_no_quest=='no') and (yes_no_quest2=='no' or yes_no_quest2=='No')):
+              print("ok let plot a graph with out name, xlabels and ylabels")
+              break
+          elif((yes_no_quest=='Yes' or yes_no_quest=='yes') and (yes_no_quest2=='no' or yes_no_quest2=='No')):
+              print("ok let plot a graph with out xlabels and ylabels")
+              break
+          else:
+              print("Wrong input!! Try again")
+#รับข้อมูล มา plot graph     
+      x = [int(i) for i in input("Enter X:").split()]
+      y = [int(j) for j in input("Enter y:").split()]
+      graph_plot(x,y)
+      
+    elif(n==2):
+        print("thank for using program")
+        break
+    else:
+        print("you input wrong number")
+#patch ล่าสุด รับชื่อ กราฟได้ รับชื่อของแกน x แกน y ได้ พอท กราฟเส้นได้
